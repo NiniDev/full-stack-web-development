a25fa70cc0b0d11e663c781100267584d3bfba73
NiniDev <linantfin@gmail.com>
2022-07-07 21:14:04 +0200

    Applyed tailwind - replaced ui

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -7,102 +7,67 @@
 </script>
 
 <style>
-	.todo {
-		width: 100%;
-		display: grid;
-		grid-template-columns: 2rem 1fr 2rem;
-		grid-gap: 0.5rem;
-		align-items: center;
-		margin: 0 0 0.5rem 0;
-		padding: 0.5rem;
-		background-color: white;
-		border-radius: 0.5rem;
-		filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
-		transform: translate(-1px, -1px);
-		transition: filter 0.2s, tranform 0.2s;
-	}
-
-	.todo button {
-		width: 2rem;
-		height: 2rem;
-		border: none;
-		background-color: transparent;
-		background-position: 50% 50%;
-		background-repeat: no-repeat;
-	}
-
-	.todo input {
-		flex: 1;
-		padding: 0.5rem 2rem 0.5rem 0.8rem;
-	}
-
-    .todo input:focus-visible {
-		flex: 1;
-		padding: 0.5rem 2rem 0.5rem 0.8rem;
-        box-shadow: none;
-        border: 1px solid rgba(0, 0, 0, 0.2);
-        border-radius: 0.5rem;
-	}
-
-	button.toggle {
-		border: 1px solid rgba(0, 0, 0, 0.2);
-		border-radius: 50%;
-		box-sizing: border-box;
-		background-size: 1em auto;
-	}
-
-	.text {
-		position: relative;
-		display: flex;
-		align-items: center;
-		flex: 1;
-	}
-
 	.save {
 		position: absolute;
 		right: 0;
 		opacity: 0;
-		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
 	}
 	.todo input:focus + .save,
 	.save:focus {
 		transition: opacity 0.2s;
 		opacity: 1;
 	}
-	.delete {
-		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.5 5V22H19.5V5H4.5Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M10 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M14 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M2 5H22' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M8 5L9.6445 2H14.3885L16 5H8Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3C/svg%3E%0A");
-		opacity: 0.2;
-	}
-	.delete:hover,
-	.delete:focus {
-		transition: opacity 0.2s;
-		opacity: 1;
-	}
-
     .done {
 		transform: none;
 		opacity: 0.4;
 		filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.1));
 	}
-
-	.done .toggle {
-		background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
-	}
 </style>
 
 
-<div class="todo" class:done={todo.done}>
-	<form action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{result: processUpdatedTodoResult}}>
-		<input type="hidden" name="done" value="{todo.done ? '' : 'true'}"/>
-		<button class="toggle"></button>
-	</form>
-
-	<form action="/todos/{todo.uid}.json?_method=patch" class="text" method="post" use:enhance={{result: processUpdatedTodoResult}}>
-		<input type="text" value="{todo.text}" name="text"/>
-		<button class="save"></button>
-	</form>
-
-	<form action="/todos/{todo.uid}.json?_method=delete" method="post" use:enhance={{result: processDeletedTodoResult}}>
-		<button class="delete"></button>
-	</form>
+<div class="p-2 sm:w-1/2 w-full todo" class:done={todo.done}>
+	<div class="bg-gray-800 rounded flex p-4 h-full items-center">
+		<form action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{result: processUpdatedTodoResult}}>
+			<input type="hidden" name="done" value="{todo.done ? '' : 'true'}"/>
+			<button class="toggle">
+				<svg
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="3"
+			class="text-blue-400 w-6 h-6 flex-shrink-0 mr-4"
+			viewBox="0 0 24 24"
+		>
+		<path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+		{#if todo.done}
+			<path d="M22 4L12 14.01l-3-3" />
+			{:else}
+			<path d="M 22 11.08 V 12 a 10 10 0 1 1 0 -1" />
+		{/if}
+		</svg>
+			</button>
+		</form>
+		<form action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{result: processUpdatedTodoResult}} class="relative flex items-center w-11/12">
+			<input type="text" value="{todo.text}" name="text" class="w-full bg-gray-800 rounded focus:border-blue-500 focus:ring-2 peer
+			focus:ring-blue-900 text-base outline-none text-gray-100
+			py-1 px-3 leading-8 transition-colors duration-200 ease-in-out title-font font-medium text-white" />
+			<button class="absolute right-0 bg-gray-800 rounded focus:border-blue-500 focus:ring-2 mr-3
+			focus:ring-blue-900 text-base outline-none text-gray-100
+			leading-8 transition-colors duration-200 ease-in-out title-font font-medium text-white save">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+				  </svg>
+			</button>
+		</form>
+		<form action="/todos/{todo.uid}.json?_method=delete" method="post" use:enhance={{result: processDeletedTodoResult}}>
+			<button class="bg-gray-800 rounded focus:border-blue-500 focus:ring-2 opacity-20 focus:opacity-80 hover:opacity-80 p-2
+			focus:ring-blue-900 text-base outline-none text-gray-100
+			leading-8 transition-colors duration-200 ease-in-out title-font font-medium text-white">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+				</svg>
+			</button>
+		</form>
+	</div>
 </div>
